1f927c1c2fad96c406e46af50318e0aca65e0780
Kaeon ACE <kaeon.ace@gmail.com>
2021-09-19 23:21:14 -0400

    Kaeon

---
+++ Repository of Kaeon/2 - Collection/1 - Implementation/1 - APIs/4 - Utilities/5 - Hardware/1 - GHI/1 - RPi - Hologram/1 - Source/ghiBoot.js
@@ -0,0 +1,6 @@
+var child = require("child-process");
+ 
+let port = process.argv.length > 2 ? process.argv[2] : 1233;
+ 
+child.exec("node ghi.js " + port);
+child.exec("sudo python3 hologram.py " + port);
