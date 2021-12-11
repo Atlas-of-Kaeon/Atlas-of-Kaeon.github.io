96b8dd5fafe0b67ef80dfa616611b4e2cfc8154f
Kaeon ACE <kaeon.ace@gmail.com>
2021-12-10 23:28:43 -0500

    Kaeon

--- Repository of Kaeon/3 - Collection/1 - Implementation/1 - APIs/4 - Utilities/1 - Software/2 - Frameworks/2 - Control/5 - Utilities/1 - Drones/1 - Identifier/1 - JavaScript/1 - Source/droneIdentifier.js
+++ Repository of Kaeon/3 - Collection/1 - Implementation/1 - APIs/4 - Utilities/1 - Software/2 - Frameworks/2 - Control/5 - Utilities/1 - Drones/1 - Identifier/1 - JavaScript/1 - Source/droneIdentifier.js
@@ -9,7 +9,7 @@ function generateUtilities(utilities) {
 	var result = JSON.parse(JSON.stringify(utilities));
 	
 	Object.keys(result).forEach((key) => {
-		result[key] = require(path);
+		result[key] = require(require(result[key]));
 	});
 
 	return result;
