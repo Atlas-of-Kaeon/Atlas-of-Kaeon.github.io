7cd3d3939b3c9a08fc0f0cee32956b7ce3091a80
Kaeon ACE <kaeon.ace@gmail.com>
2021-12-11 13:30:56 -0500

    Kaeon

--- Repository of Kaeon/3 - Collection/1 - Implementation/1 - APIs/4 - Utilities/1 - Software/2 - Frameworks/2 - Control/6 - Scripts/1 - Main/1 - JavaScript/1 - Source/controlMainScript.js
+++ Repository of Kaeon/3 - Collection/1 - Implementation/1 - APIs/4 - Utilities/1 - Software/2 - Frameworks/2 - Control/6 - Scripts/1 - Main/1 - JavaScript/1 - Source/controlMainScript.js
@@ -12,32 +12,44 @@ if(metadata.schedule.sequences == null)
 if(metadata.schedule.intervals == null)
 	metadata.schedule.intervals = [];
 
+let active = false;
+
 let newState = JSON.parse(JSON.stringify(state));
 
-metadata.schedule.sequences.forEach((sequence) => {
+for(let i = 0; i < metadata.schedule.sequences.length; i++) {
 
-	if(sequence.delta == null)
-		sequence.delta = 0;
-
-	if(sequence.sequence == null)
-		sequence.sequence = [];
+	if(metadata.schedule.sequences[i].delta == null)
+		metadata.schedule.sequences[i].delta = 0;
 	
-	sequence.delta -= delta / 1000;
-
-	if(sequence.sequence.length == 0 || sequence.delta > 0)
+	metadata.schedule.sequences[i].delta -= delta / 1000;
+	
+	if(metadata.schedule.sequences[i].delta > 0)
 		return;
 
-	let operation = sequence.sequence[0].operation;
+	active = true;
+	
+	let operation = metadata.schedule.sequences[i].sequence[0].operation;
+	
+	if(metadata.schedule.sequences[i].sequence[0].delta != null) {
 
-	if(sequence.sequence[0].delta != null)
-		sequence.delta = sequence.sequence[0].delta;
-
-	sequence.sequence = sequence.sequence.slice(1);
-
+		metadata.schedule.sequences[i].delta =
+			metadata.schedule.sequences[i].sequence[0].delta;
+	}
+	
+	metadata.schedule.sequences[i].sequence =
+		metadata.schedule.sequences[i].sequence.slice(1);
+	
 	Object.keys(operation).forEach((key) => {
 		Object.assign(newState[key], operation[key]);
 	});
-});
+	
+	if(metadata.schedule.sequences[i].sequence.length == 0) {
+
+		metadata.schedule.sequences.splice(i, 1);
+
+		i--;
+	}
+}
 
 metadata.schedule.intervals.forEach((interval) => {
 
@@ -46,18 +58,26 @@ metadata.schedule.intervals.forEach((interval) => {
 
 	if(interval.deltaCount == null)
 		interval.deltaCount = 0;
-
+	
 	interval.deltaCount -= delta / 1000;
-
+	
 	if(interval.deltaCount > 0)
 		return null;
+
+	active = true;
 
 	interval.deltaCount = interval.delta;
 
 	if(interval.operation == null)
 		interval.operation = "";
-
+	
 	Object.assign((new Function(interval.operation))(state), newState);
 });
 
-return JSON.stringify(newState);
+if(active) {
+
+	newState["0"].output.metadata =
+		JSON.parse(JSON.stringify(state["0"].output.metadata));
+	
+	return JSON.stringify(newState, null, "\t");
+}
