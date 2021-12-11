c6f2cd9a814f2932ff3ba5fb9d2fdb6efb5e0d9d
Kaeon ACE <kaeon.ace@gmail.com>
2021-12-10 23:26:35 -0500

    Kaeon

--- Repository of Kaeon/3 - Collection/1 - Implementation/1 - APIs/4 - Utilities/1 - Software/2 - Frameworks/2 - Control/4 - Commands/3 - Drone/1 - Launch/1 - JavaScript/1-Source/controlDroneLaunch.js
+++ Repository of Kaeon/3 - Collection/1 - Implementation/1 - APIs/4 - Utilities/1 - Software/2 - Frameworks/2 - Control/4 - Commands/3 - Drone/1 - Launch/1 - JavaScript/1-Source/controlDroneLaunch.js
@@ -2,6 +2,8 @@ moduleDependencies = {
 	droneIdentifier: "https://raw.githubusercontent.com/Atlas-of-Kaeon/Atlas-of-Kaeon.github.io/master/Repository%20of%20Kaeon/3%20-%20Collection/1%20-%20Implementation/1%20-%20APIs/4%20-%20Utilities/1%20-%20Software/2%20-%20Frameworks/2%20-%20Control/5%20-%20Utilities/1%20-%20Drones/1%20-%20Identifier/1%20-%20JavaScript/1%20-%20Source/droneIdentifier.js"
 };
 
+var droneIdentifier = require(moduleDependencies.droneIdentifier);
+
 module.exports = (devices, operation, message, state) => {
 
 	if(devices.receptor == null || devices.wifi == null)
