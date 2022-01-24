f6cf1b7728899bcc9f33e55eb1b9a3b5fd9f2d05
Kaeon ACE <kaeon.ace@gmail.com>
2022-01-24 18:40:20 -0500

    Kaeon

--- Repository of Kaeon/3 - Collection/1 - Implementation/1 - APIs/2 - Kaeon Series/2 - Utilities/2 - Hardware/2 - GHI/1 - Source/2 - Adapters/1 - Raspberry Pi/1 - Application/ghiSource.js
+++ Repository of Kaeon/3 - Collection/1 - Implementation/1 - APIs/2 - Kaeon Series/2 - Utilities/2 - Hardware/2 - GHI/1 - Source/2 - Adapters/1 - Raspberry Pi/1 - Application/ghiSource.js
@@ -1,11 +1,48 @@
 var child = require("child_process");
+var fs = require("fs");
 
-let port = process.argv.length > 2 ?
-	process.argv[2] : 1233;
+let dataGHI = { };
+
+try {
+	dataGHI = JSON.parse(fs.readFileSync(__dirname + "/dataGHI.json"));
+}
+
+catch(error) {
+	dataGHI = { };
+}
+
+if(dataGHI.access == null)
+	dataGHI.access = { };
+
+let port = dataGHI.access.port != null ?
+	dataGHI.access.port :
+	(process.argv.length > 2 ?
+		process.argv[2] :
+		1233
+	);
 
 let wifiPort = process.argv.length > 3 ?
 	process.argv[3] : (port != 1234 ? 1234 : 1233);
 
-child.exec("sudo /usr/local/bin/node " + __dirname + "/ghiServerWifi.js " + wifiPort);
-child.exec("sudo /usr/local/bin/node " + __dirname + "/ghiServerRouter.js " + port + " " + wifiPort);
-child.exec("sudo /bin/python3 " + __dirname + "/uhapi.py " + port);
+child.exec(
+	"sudo /usr/local/bin/node " +
+	__dirname +
+	"/ghiServerWifi.js " +
+	wifiPort
+);
+
+child.exec(
+	"sudo /usr/local/bin/node " +
+	__dirname +
+	"/ghiServerRouter.js " +
+	port +
+	" " +
+	wifiPort
+);
+
+child.exec(
+	"sudo /bin/python3 " +
+	__dirname +
+	"/uhapi.py " +
+	port
+);
