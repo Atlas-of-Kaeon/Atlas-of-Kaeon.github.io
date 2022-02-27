b947b9b840a8277ef1d9e4c744838d2edc672efb
Kaeon ACE <kaeon.ace@gmail.com>
2022-02-27 03:44:40 -0500

    Kaeon

--- Repository of Kaeon/3 - Collection/1 - Implementation/1 - APIs/2 - Kaeon Series/1 - ONE/3 - Kaeon ACE/1 - APIs/1 - Babylon/1 - Core/1 - Source/KaeonACECore.js
+++ Repository of Kaeon/3 - Collection/1 - Implementation/1 - APIs/2 - Kaeon Series/1 - ONE/3 - Kaeon ACE/1 - APIs/1 - Babylon/1 - Core/1 - Source/KaeonACECore.js
@@ -1,5 +1,3 @@
-const { create } = require("../../../../../../../1 - Core/1 - ONE/1 - ONE/1 - JavaScript/1 - Source/ONE");
-
 var moduleDependencies = {
 	one: "https://raw.githubusercontent.com/Atlas-of-Kaeon/Atlas-of-Kaeon.github.io/master/Repository%20of%20Kaeon/3%20-%20Collection/1%20-%20Implementation/1%20-%20APIs/1%20-%20Core/1%20-%20ONE/1%20-%20ONE/1%20-%20JavaScript/1%20-%20Source/ONE.js",
 	philosophersStone: "https://raw.githubusercontent.com/Atlas-of-Kaeon/Atlas-of-Kaeon.github.io/master/Repository%20of%20Kaeon/3%20-%20Collection/1%20-%20Implementation/1%20-%20APIs/1%20-%20Core/2%20-%20Philosopher's%20Stone/1%20-%20Philosopher's%20Stone/1%20-%20JavaScript/1%20-%20Source/PhilosophersStone.js",
