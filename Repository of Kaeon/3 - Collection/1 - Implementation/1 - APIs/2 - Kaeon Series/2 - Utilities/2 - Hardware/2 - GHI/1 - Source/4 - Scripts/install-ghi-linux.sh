5ef409c679fc38b35bc2b87e2218ff10f61b4f45
Kaeon ACE <kaeon.ace@gmail.com>
2022-03-20 09:14:44 -0400

    Kaeon

---
+++ Repository of Kaeon/3 - Collection/1 - Implementation/1 - APIs/2 - Kaeon Series/2 - Utilities/2 - Hardware/2 - GHI/1 - Source/4 - Scripts/install-ghi-linux.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo curl -s -L https://raw.githubusercontent.com/Atlas-of-Kaeon/Atlas-of-Kaeon.github.io/master/Repository%20of%20Kaeon/3%20-%20Collection/1%20-%20Implementation/1%20-%20APIs/3%20-%20United/1%20-%20Resources/4%20-%20Scripts/install-node-linux.sh | bash
+sudo npm install -g ghi-server
+
+if [$1 == "enable"]
+	then
+		sudo ghi enable
+	fi
