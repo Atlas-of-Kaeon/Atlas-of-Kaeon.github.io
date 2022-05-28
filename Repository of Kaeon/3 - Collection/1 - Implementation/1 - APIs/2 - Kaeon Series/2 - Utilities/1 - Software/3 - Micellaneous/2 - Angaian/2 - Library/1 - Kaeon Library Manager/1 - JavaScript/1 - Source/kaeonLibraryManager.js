c31511eead445eb0e752c7aafc86e7e7b1fc9f14
unknown <kaeon.ace@gmail.com>
2022-05-27 20:47:26 -0400

    Kaeon

--- Repository of Kaeon/3 - Collection/1 - Implementation/1 - APIs/2 - Kaeon Series/2 - Utilities/1 - Software/3 - Micellaneous/2 - Angaian/2 - Library/1 - Kaeon Library Manager/1 - JavaScript/1 - Source/kaeonLibraryManager.js
+++ Repository of Kaeon/3 - Collection/1 - Implementation/1 - APIs/2 - Kaeon Series/2 - Utilities/1 - Software/3 - Micellaneous/2 - Angaian/2 - Library/1 - Kaeon Library Manager/1 - JavaScript/1 - Source/kaeonLibraryManager.js
@@ -1,8 +1,10 @@
 var moduleDependencies = {
-	repoExplorer: "https://raw.githubusercontent.com/Atlas-of-Kaeon/Atlas-of-Kaeon.github.io/master/Repository%20of%20Kaeon/3%20-%20Collection/1%20-%20Implementation/1%20-%20APIs/2%20-%20Kaeon%20Series/2%20-%20Utilities/1%20-%20Software/1%20-%20Kaeon%20APIs/1%20-%20General/1%20-%20Modules/1%20-%20Data/3%20-%20API/2%20-%20Repo%20Explorer/1%20-%20JavaScript/1%20-%20Source/repoExplorer.js"
+	repoExplorer: "https://raw.githubusercontent.com/Atlas-of-Kaeon/Atlas-of-Kaeon.github.io/master/Repository%20of%20Kaeon/3%20-%20Collection/1%20-%20Implementation/1%20-%20APIs/2%20-%20Kaeon%20Series/2%20-%20Utilities/1%20-%20Software/1%20-%20Kaeon%20APIs/1%20-%20General/1%20-%20Modules/1%20-%20Data/3%20-%20API/2%20-%20Repo%20Explorer/1%20-%20JavaScript/1%20-%20Source/repoExplorer.js",
+	wrapONE: "https://raw.githubusercontent.com/Atlas-of-Kaeon/Atlas-of-Kaeon.github.io/master/Repository%20of%20Kaeon/3%20-%20Collection/1%20-%20Implementation/1%20-%20APIs/2%20-%20Kaeon%20Series/2%20-%20Utilities/1%20-%20Software/1%20-%20Kaeon%20APIs/1%20-%20General/1%20-%20Modules/1%20-%20Data/2%20-%20Parsing/2%20-%20Utilities/1%20-%20ONE/1%20-%20Wrap%20ONE/1%20-%20Javascript/1%20-%20Source/wrapONE.js"
 };
 
 var repoExplorer = require(moduleDependencies.repoExplorer);
+var wrapONE = require(moduleDependencies.wrapONE);
 
 function renderFolder(protocol, path) {
 
@@ -12,7 +14,7 @@ function renderFolder(protocol, path) {
 	if(items.files.length == 1) {
 
 		render =
-			"#[ " + path.join(": ") + "]#\n\n" +
+			"#[ " + path.join(": ") + " ]#\n\n" +
 			repoExplorer.getItem(protocol, path.concat([items.files[0]])) +
 			"\n\n";
 	}
@@ -32,20 +34,24 @@ function renderLibrary(protocol, path) {
 	if(items.files.length == 1) {
 
 		render =
-			"#[ " + path.join(": ") + "]#\n\n" +
+			"#[ " + path.join(": ") + " ]#\n\n" +
 			repoExplorer.getItem(protocol, path.concat([items.files[0]])) +
 			"\n\n";
 	}
 
 	items.folders.forEach((item) => {
 
-		if(!(item == "1 - Core" || item == "1 - Connections" || item == "2 - Connections"))
+		if(!(item == "1 - Core" ||
+			item == "1 - Connections" ||
+			item == "2 - Connections")) {
+			
 			return;
+		}
 
 		render += renderFolder(protocol, path.concat([item])) + "\n\n";
 	});
 
-	return render.trim();
+	return wrapONE.wrap(render.trim(), 99);
 }
 
 module.exports = {
