8a2c5281c89b1adcd0d81c1e45dd6e38b14efb25
unknown <kaeon.ace@gmail.com>
2023-01-12 19:28:00 -0500

    Kaeon

--- Kaeon United/2 - Wonders/2 - Source/2 - System/1 - Resources/1 - Interfaces/2 - General Interface/2 - APIs/1 - Services/5 - You/youUtils.js
+++ Kaeon United/2 - Wonders/2 - Source/2 - System/1 - Resources/1 - Interfaces/2 - General Interface/2 - APIs/1 - Services/5 - You/youUtils.js
@@ -80,17 +80,76 @@ function parseResponse(data) {
 	}).join("");
 }
 
+function search(query, limit) {
+
+	return searchExecute(
+		query, limit, 10, "search", "search", "bing_search_results"
+	);
+}
+
+function searchExecute(query, limit, increment, domain, object, list) {
+
+	let pages = Math.ceil((limit != null ? limit : increment) / increment);
+
+	let results = [];
+
+	for(let i = 0; i < pages; i++) {
+
+		results = results.concat(
+			open(
+				"https://you.com/api/streamingSearch?q=" +
+					encodeURIComponent(query) +
+					"&page=" +
+					i +
+					"&count=" +
+					(pages * increment) +
+					"&domain=" +
+					domain
+			).trim().split("\n\n").map(item => {
+				
+				item = item.split("\n");
+				
+				if(item[0] != "event: thirdPartySearchResults")
+					return [];
+	
+				return JSON.parse(
+					item[1].trim().substring(5).trim()
+				)[object][list].map(item => item.url);
+			}).flat()
+		);
+	}
+
+	return results.slice(0, limit);
+
+}
+
+function searchImages(query, limit) {
+
+	return searchExecute(
+		query, limit, 30, "image", "image", "results"
+	);
+}
+
 module.exports = {
 	methods: {
 		chat,
+		open,
 		parseResponse,
-		open
+		search,
+		searchImages
 	},
 	interfaces: {
 		chat: {
 			name: "you",
 			methods: {
 				chat
+			}
+		},
+		search: {
+			name: "you",
+			methods: {
+				search: search,
+				searchImages: searchImages
 			}
 		}
 	}
