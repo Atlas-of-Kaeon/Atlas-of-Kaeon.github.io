2d5751c56b263506704c755253deee1ea0978ef0
unknown <kaeon.ace@gmail.com>
2023-10-22 16:52:22 -0400

    Kaeon

--- Kaeon United/3 - Wonders/2 - Source/1 - Code/2 - APIs/1 - United/1 - Core/1 - ONE/8 - ACE/aceUtils.js
+++ Kaeon United/3 - Wonders/2 - Source/1 - Code/2 - APIs/1 - United/1 - Core/1 - ONE/8 - ACE/aceUtils.js
@@ -8,7 +8,10 @@ function formatDocument(document) {
 	if(typeof document == "string") {
 
 		try {
-			return formatConverter.oneToJSON(formatConverter.jsonToONE(JSON.parse(document)));
+
+			return formatConverter.oneToJSON(
+				formatConverter.jsonToONE(JSON.parse(document))
+			);
 		}
 
 		catch(error) {
@@ -33,17 +36,76 @@ function formatEntity(entity) {
 	return entity;
 }
 
-function formatKaeonACE(document, open) {
+function formatKaeonACE(document, open, globalMap, meta, alias) {
 
-	if(document.components != null) {
-		// STUB
+	formatEntity(document);
+
+	globalMap = globalMap != null ? globalMap : { };
+	meta = meta != null ? JSON.parse(JSON.stringify(meta)) : { };
+
+	let components = getValue(document, "components");
+	let entities = getValue(document, "entities");
+
+	let voidComponent = getValue(components, "void");
+	
+	if(voidComponent != null) {
+
+		if(Object.keys(voidComponent).length == 0)
+			meta = { };
+
+		else {
+
+			Object.keys(voidComponent).forEach((key) => {
+				removeValue(components, key);
+			});
+		}
 	}
 
-	if(document.entities != null) {
+	overlayComponents(
+		components,
+		overlayComponents(
+			meta,
+			getValue(components, "meta") != null ?
+				getValue(components, "meta") : { }
+		),
+		false,
+		true
+	);
 
-		Object.keys(document.entities).forEach(
-			key => formatKaeonACE(document.entities[key])
-		);
+	overlayEntity(
+		document,
+		queryKaeonACE(
+			getValue(components, "ace") != null ?
+				getValue(components, "ace") : { },
+			open
+		)
+	);
+
+	if(getValue(components, "meta") != null)
+		removeValue(components, "meta");
+
+	if(alias != null) {
+
+		if(globalMap[alias] == null)
+			globalMap[alias] = { components: { }, entities: [] };
+
+		overlayComponents(globalMap[alias].components, components);
+
+		globalMap[alias].entities.push(document);
+	}
+
+	Object.keys(entities).forEach(
+		key => formatKaeonACE(entities[key], open, globalMap, meta, key)
+	);
+
+	if(alias == null) {
+		
+		Object.values(globalMap).forEach((item) => {
+
+			item.entities.forEach((entity) => {
+				overlayComponents(entity, item.components, false, true);
+			});
+		});
 	}
 
 	return document;
@@ -86,39 +148,105 @@ function isONE(element) {
 	}
 }
 
-function overlayComponents(target, source) {
+function overlayComponents(target, source, clone, priority) {
+
+	if(clone) {
+		target = JSON.parse(JSON.stringify(target));
+		source = JSON.parse(JSON.stringify(source));
+	}
 
 	Object.keys(source).forEach((key) => {
 
 		if(getValue(target, key) == null)
 			target[key] = getValue(source, key);
 
-		else
+		else if(!priority)
 			Object.assign(getValue(target, key), getValue(source, key));
 	});
+
+	return target;
 }
 
-function overlayEntity(target, source) {
+function overlayEntity(target, source, clone) {
+
+	if(clone) {
+		target = JSON.parse(JSON.stringify(target));
+		source = JSON.parse(JSON.stringify(source));
+	}
 
 	[target, source].forEach(item => formatEntity(item));
 
 	let targetEntities = getValue(target, "entities");
 	let sourceEntities = getValue(source, "entities");
 
-	overlayComponents(getValue(target, "components"), getValue(source, "components"));
+	overlayComponents(
+		getValue(target, "components"),
+		getValue(source, "components")
+	);
 			
 	Object.keys(sourceEntities).forEach((key) => {
 
 		if(getValue(targetEntities, key) == null)
 			targetEntities[key] = getValue(sourceEntities, key);
 
-		else
-			overlayEntity(getValue(targetEntities, key), getValue(sourceEntities, key));
+		else {
+
+			overlayEntity(
+				getValue(targetEntities, key),
+				getValue(sourceEntities, key)
+			);
+		}
 	});
+
+	return target;
 }
 
-function traceKaeonACE(document, criteria) {
+function queryKaeonACE(entity, open) {
+
+	open = open != null ? open : io.open;
+
 	// STUB
+
+	return entity;
+}
+
+function removeValue(object, key) {
+
+	delete object[Object.keys(object).filter(
+		item => item.toLowerCase().trim() == key.toLowerCase().trim()
+	)[0]];
+}
+
+function traceKaeonACE(document, criteria, alias) {
+
+	let components = getValue(document, "components");
+	let entities = getValue(document, "entities");
+
+	if(criteria.alias == null || criteria.alias == alias) {
+
+		if(components != null && criteria[0].components != null) {
+	
+			if(JSON.stringify(components) == JSON.stringify(
+				overlayComponents(components, criteria[0].components, true))) {
+				
+				criteria.splice(0, 1);
+		
+				if(criteria.length == 0)
+					return [document];
+			}
+		}
+	}
+
+	if(entities == null)
+		return [];
+
+	let results = [];
+
+	Object.keys(entities).forEach((key) => {
+		results = results.concat(traceKaeonACE(entities[key], criteria, key));
+	});
+
+	return results;
 }
 
 function useACE() {
@@ -127,12 +255,17 @@ function useACE() {
 	let documents = [];
 	
 	Array.from(arguments).forEach((argument) => {
-		documents = documents.concat(Array.isArray(argument) ? argument : [argument]);
+
+		documents = documents.concat(
+			Array.isArray(argument) ? argument : [argument]
+		);
 	});
 
-	documents.map(item => getUsage(formatDocument(item))).flat().forEach((item) => {
-		overlayEntity(document, item);
-	});
+	documents.map(item => getUsage(formatDocument(item))).flat().forEach(
+		(item) => {
+			overlayEntity(document, item);
+		}
+	);
 
 	return document;
 }
@@ -144,5 +277,6 @@ module.exports = {
 	isONE,
 	traceKaeonACE,
 	overlayEntity,
+	queryKaeonACE,
 	useACE
 };
